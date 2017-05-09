d269d732fcd627be78840ce2331acdd3b45b632e
Joachim van der Herten <joachim.vanderherten@ugent.be>
2017-05-09 16:29:16 +0200

    Implementation of transforms to facilitate easy data scaling.

--- GPflowOpt/domain.py
+++ GPflowOpt/domain.py
@@ -14,8 +14,9 @@
 
 import numpy as np
 from itertools import chain
-
 from GPflow.param import Parentable
+
+from .transforms import LinearTransform
 
 
 class Domain(Parentable):
@@ -34,10 +35,6 @@ class Domain(Parentable):
     @property
     def upper(self):
         return np.array(list(map(lambda param: param.upper, self._parameters))).flatten()
-
-    # def optimize(self, optimizer, objectivefx):
-    #    optimizer.domain = self
-    #    result = optimizer.optimize(objectivefx)
 
     def __add__(self, other):
         assert isinstance(other, Domain)
@@ -71,6 +68,12 @@ class Domain(Parentable):
 
     def __getitem__(self, item):
         return self._parameters[item]
+
+    def __rshift__(self, other):
+        assert(self.size == other.size)
+        A = (other.upper - other.lower) / (self.upper - self.lower)
+        b = -self.upper * A + other.upper
+        return LinearTransform(A, b)
 
     @property
     def value(self):
@@ -117,7 +120,7 @@ class Parameter(Domain):
 
 class ContinuousParameter(Parameter):
     def __init__(self, label, lb, ub, xinit=None):
-        self._range = [lb, ub]
+        self._range = np.array([lb, ub], dtype=float)
         super(ContinuousParameter, self).__init__(label, xinit or ((ub + lb) / 2.0))
 
     @Parameter.lower.getter
---
+++ testing/test_transforms.py
@@ -0,0 +1,65 @@
+import unittest
+import GPflow
+import GPflowOpt
+import tensorflow as tf
+from GPflow import settings
+import numpy as np
+
+float_type = settings.dtypes.float_type
+np_float_type = np.float32 if float_type is tf.float32 else np.float64
+
+
+class LinearTransformTests(unittest.TestCase):
+    """
+    Tests are inspired on GPflow transform tests.
+    """
+
+    def setUp(self):
+        tf.reset_default_graph()
+        self.x = tf.placeholder(float_type)
+        self.y = tf.placeholder(float_type)
+
+        self.x_np = np.random.rand(10, 2).astype(np_float_type)
+        self.session = tf.Session()
+        self.transforms = [GPflowOpt.transforms.LinearTransform([2.0, 3.5], [1.2, 0.7])]
+
+    def test_tf_np_forward(self):
+        ys = [t.tf_forward(self.x) for t in self.transforms]
+        ys_tf = [self.session.run(y, feed_dict={self.x: self.x_np}) for y in ys]
+        ys_np = [t.forward(self.x_np) for t in self.transforms]
+        for y1, y2 in zip(ys_tf, ys_np):
+            self.assertTrue(np.allclose(y1, y2))
+
+    def test_forward_backward(self):
+        ys_np = [t.forward(self.x_np) for t in self.transforms]
+        xs_np = [t.backward(y) for t, y in zip(self.transforms, ys_np)]
+        for x in xs_np:
+            self.assertTrue(np.allclose(x, self.x_np))
+
+    def test_tf_forward_backward(self):
+        ys = [t.tf_forward(self.x) for t in self.transforms]
+        xs = [t.tf_backward(y) for t, y in zip(self.transforms, ys)]
+        xs_tf = [self.session.run(x, feed_dict={self.x: self.x_np}) for x in xs]
+        for x in xs_tf:
+            self.assertTrue(np.allclose(x, self.x_np))
+
+    def test_invert_np(self):
+        ys_np = [t.forward(self.x_np) for t in self.transforms]
+        xs_np = [t.backward(y) for t, y in zip(self.transforms, ys_np)]
+        xsi_np = [(~t).forward(y) for t, y in zip(self.transforms, ys_np)]
+
+        for x in zip(xs_np, xsi_np):
+            self.assertTrue(np.allclose(x[0], self.x_np))
+            self.assertTrue(np.allclose(x[1], self.x_np))
+            self.assertTrue(np.allclose(x[0], x[1]))
+
+    def test_invert_tf(self):
+        ys = [t.tf_forward(self.x) for t in self.transforms]
+        xs = [t.tf_backward(y) for t, y in zip(self.transforms, ys)]
+        xsi = [(~t).tf_forward(y) for t, y in zip(self.transforms, ys)]
+        xs_tf = [self.session.run(x, feed_dict={self.x: self.x_np}) for x in zip(xs, xsi)]
+
+        for x in xs_tf:
+            self.assertTrue(np.allclose(x[0], self.x_np))
+            self.assertTrue(np.allclose(x[1], self.x_np))
+            self.assertTrue(np.allclose(x[0], x[1]))
