823cb48091d3aa8f2686623ae7c757a17cf83453
Joachim van der Herten <joachim.vanderherten@ugent.be>
2017-06-03 23:58:53 +0200

    Added some parameters and docstring to PoF

--- GPflowOpt/acquisition.py
+++ GPflowOpt/acquisition.py
@@ -170,21 +170,43 @@ class ProbabilityOfFeasibility(Acquisition):
     Probability of Feasibility acquisition function for learning  feasible regions
     """
 
-    def __init__(self, model):
+    def __init__(self, model, threshold=0.0, minimum_pof=0.5):
+        """
+
+        :param model: GPflow model (single output) for computing the PoF
+        :param threshold: threshold value. Observed values lower than this value are considered valid
+        :param minimum_pof: minimum pof score required for a point to be valid. For more information, see docstring
+        of feasible_data_index
+        """
         super(ProbabilityOfFeasibility, self).__init__(model)
+        self.threshold = threshold
+        self.minimum_pof = minimum_pof
 
     def constraint_indices(self):
         return np.arange(self.data[1].shape[1])
 
     def feasible_data_index(self):
+        """
+        Returns a boolean array indicating which points are feasible (True) and which are not (False)
+        Answering the question *which points are feasible?* is slightly troublesome in case noise is present.
+        Directly relying on the data and comparing it to self.threshold can be troublesome.
+
+        Instead, we rely on the model belief. More specifically, we evaluate the PoF (score between 0 and 1).
+        As the implementation of the PoF corresponds to the cdf of the (normal) predictive distribution in
+        a point evaluated at the threshold, requiring a minimum pof of 0.5 implies the mean of the predictive
+        distribution is below the threshold, hence it is marked as feasible. A minimum pof of 0 marks all points valid.
+        Setting it to 1 results in all invalid.
+        :return: boolean ndarray, size N
+        """
+        # In
         pred = self.evaluate(self.data[0])
-        return pred.ravel() > 0.5
+        return pred.ravel() > self.minimum_pof
 
     def build_acquisition(self, Xcand):
         candidate_mean, candidate_var = self.models[0].build_predict(Xcand)
         candidate_var = tf.maximum(candidate_var, stability)
         normal = tf.contrib.distributions.Normal(candidate_mean, tf.sqrt(candidate_var))
-        return normal.cdf(tf.constant(0.0, dtype=float_type), name=self.__class__.__name__)
+        return normal.cdf(tf.constant(self.threshold, dtype=float_type), name=self.__class__.__name__)
 
 
 class ProbabilityOfImprovement(Acquisition):
