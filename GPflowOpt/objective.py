5bc487a42f2586ca8d433d957e42983e32b50c29
Joachim van der Herten <joachim.vanderherten@ugent.be>
2017-07-25 01:09:37 +0200

    Changes to tests, updated docs

--- GPflowOpt/objective.py
+++ GPflowOpt/objective.py
@@ -13,9 +13,22 @@
 # limitations under the License.
 import numpy as np
 from functools import wraps
+from GPflow import model
 
 
 def batch_apply(fun):
+    """
+    Decorator which applies a function along the first dimension of a given ndarray as argument (the batch dimension)
+    the most common use case is a function which is to convert a function designed to operate on an input vector, and
+    to compute its response (and possibly gradient) for each row of a matrix.
+
+    :param fun: function accepting an input vector of dimensionality d and returns a vector of dimensionality p (the
+       output dimensionality, usually one) and (optionally) a gradient of size d x p. If p is 1, the extra dimension is
+       not supposed to be returned.
+    :return: a function wrapper which calls fun on each row of a given n x d matrix. Here n represents the batch
+       dimension. the wrapper returns n x p and optionally a n x d x p matrix (again, if p is 1 the extra dimension is
+       not returned)
+    """
     @wraps(fun)
     def batch_wrapper(X):
         responses = (fun(x) for x in np.atleast_2d(X))
@@ -24,19 +37,43 @@ def batch_apply(fun):
         if len(sep) == 1:
             return f
 
-        g_stacked = np.stack((np.atleast_2d(r) for r in sep[1]), axis=0)  # n x p x d
-        print('results: ')
-        print(sep[1])
-        g = np.squeeze(g_stacked, axis=1)  # in case p = 1
+        # for each point, the gradient is either (d,) or (d, p) shaped.
+        g_stacked = np.stack((r for r in sep[1]), axis=0)  # n x d or n x d x p
+        # Get rid of last dim = 1 in case p = 1
+        g = np.squeeze(g_stacked, axis=2) if len(g_stacked.shape) == 3 and g_stacked.shape[2] == 1 else g_stacked
         return f, g
 
     return batch_wrapper
 
 
 def to_args(fun):
+    """
+    Decorator for calling an objective function which has each feature as seperate input parameter. The 2d input ndarray
+    is split column wise and passed as arguments. Can be combined with batch apply.
+    
+    :param fun: function accepting d n-dimensional vectors (each representing a feature and returns a a matrix of
+       dimensionality n x p and optionally a gradient of size n x d x p (or n x d if p == 1)
+    :return: a function wrapper which splits a given input ndarray into its columns to call fun.
+    """
     @wraps(fun)
     def args_wrapper(X):
         X = np.atleast_2d(X)
         return fun(*X.T)
 
     return args_wrapper
+
+
+class ObjectiveWrapper(model.ObjectiveWrapper):
+    def __init__(self, objective, exclude_gradient):
+        super(ObjectiveWrapper, self).__init__(objective)
+        self._no_gradient = exclude_gradient
+        self.counter = 0
+
+    def __call__(self, x):
+        x = np.atleast_2d(x)
+        f, g = super(ObjectiveWrapper, self).__call__(x)
+        self.counter += x.shape[0]
+        if self._no_gradient:
+            return f
+        return f, g
+
