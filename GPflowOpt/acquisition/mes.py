91a5e66772bf54e78f5e12ec2d86a0b84f4bfbaa
nknudde <nicolas.knudde@ugent.be>
2017-08-10 23:53:38 +0100

    MES Gumbel sampling

---
+++ GPflowOpt/acquisition/mes.py
@@ -0,0 +1,82 @@
+# Copyright 2017 Joachim van der Herten
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .acquisition import Acquisition
+
+from GPflow import settings
+from GPflow.param import DataHolder
+
+import numpy as np
+from scipy.stats import norm
+import tensorflow as tf
+
+float_type = settings.dtypes.float_type
+stability = settings.numerics.jitter_level
+
+
+class MaxvalueEntropySearch(Acquisition):
+    def __init__(self, model, domain, gridsize=10000, num_samples=10):
+        super(MaxvalueEntropySearch, self).__init__(model, domain=domain)
+        self.gridsize = gridsize
+        self.num_samples = num_samples
+        self.samples = DataHolder(np.zeros(num_samples))
+
+    def setup(self):
+        super(MaxvalueEntropySearch, self).setup()
+        m = self.models[0].wrapped
+        X = np.array(m.X.value)
+        D = np.shape(X)[1]
+
+        Xrand = np.random.rand(self.gridsize, D)
+        fmean, fvar = m.predict_f(np.concatenate((Xrand, X), axis=0))
+
+        right = np.max(m.Y.value)
+
+        probf = lambda x: np.exp(np.sum(norm.logcdf(-(x - fmean) / np.sqrt(fvar))))
+
+        left = np.min(fmean - 5 * np.sqrt(fvar))
+        while probf(left) < 0.75:
+            left = -2 * left + right
+
+        q1 = self.binary_search(left, right, probf, 0.25)
+        q2 = self.binary_search(left, right, probf, 0.75)
+        med = self.binary_search(left, right, probf, 0.5)
+        beta = (q1 - q2) / (np.log(np.log(4 / 3)) - np.log(np.log(4)))
+        alpha = med + beta * np.log(np.log(2))
+        mins = - np.log(-np.log(np.random.rand(self.num_samples))) * beta + alpha
+
+        self.samples.set_data(mins)
+
+    def binary_search(self, left, right, func, val, threshold=0.01):
+        vfunc = np.vectorize(func)
+        x = np.flip(np.linspace(left, right, 100), axis=0)
+        y = vfunc(x)
+        print(y)
+        i = np.searchsorted(y, val)
+        l, r = x[i], x[i - 1]
+        mid = (l + r) / 2
+        ev = func(mid)
+        while np.abs(ev - val) > threshold:
+            if ev > val:
+                l = mid
+                r = r
+            else:
+                l = l
+                r = mid
+            mid = (l + r) / 2
+            ev = func(mid)
+
+        return mid
+
+    def build_acquisition(self, Xcand):
