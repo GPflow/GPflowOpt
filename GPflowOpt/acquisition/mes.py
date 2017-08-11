deedd2bc81e1e444efc7e9678e7a1596431ef559
Joachim van der Herten <joachim.vanderherten@ugent.be>
2017-08-11 07:38:36 +0200

    Got rid of scalings

--- GPflowOpt/acquisition/mes.py
+++ GPflowOpt/acquisition/mes.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Joachim van der Herten
+# Copyright 2017 Joachim van der Herten, Nicolas Knudde
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from .acquisition import Acquisition
+from ..design import RandomDesign
 
 from GPflow import settings
 from GPflow.param import DataHolder
@@ -20,8 +21,6 @@ from GPflow.param import DataHolder
 import numpy as np
 from scipy.stats import norm
 import tensorflow as tf
-
-from functools import partial
 
 float_type = settings.dtypes.float_type
 stability = settings.numerics.jitter_level
@@ -50,33 +49,31 @@ class MaxvalueEntropySearch(Acquisition):
             }
         """
     def __init__(self, model, domain, gridsize=10000, num_samples=10):
-        super(MaxvalueEntropySearch, self).__init__(model, domain=domain)
+        super(MaxvalueEntropySearch, self).__init__(model)
         self.gridsize = gridsize
         self.num_samples = num_samples
         self.samples = DataHolder(np.zeros(num_samples))
+        self._domain = domain
 
     def setup(self):
         super(MaxvalueEntropySearch, self).setup()
-        m = self.models[0].wrapped
-        X = np.array(m.X.value.copy())
-        D = np.shape(X)[1]
-
-        Xrand = np.random.rand(self.gridsize, D)
-        fmean, fvar = m.predict_f(np.concatenate((Xrand, X), axis=0))
-
-        right = np.max(m.Y.value.copy())
+        m = self.models[0]
+        X = m.X.value
+        Xrand = RandomDesign(self.gridsize, self._domain).generate()
+        fmean, fvar = m.predict_f(np.vstack((Xrand, X)))
 
         probf = lambda x: np.exp(np.sum(norm.logcdf(-(x - fmean) / np.sqrt(fvar)), axis=0))
 
         left = np.min(fmean - 5 * np.sqrt(fvar))
+        right = np.max(m.Y.value)
+
         while probf(left) < 0.75:
             left = -2 * left + right
 
-        q1, med, q2 = map(partial(self.__class__.binary_search, self, left, right, probf), [0.25, 0.5, 0.75])
+        q1, med, q2 = map(lambda val: self.binary_search(left, right, probf, val), [0.25, 0.5, 0.75])
         beta = (q1 - q2) / (np.log(np.log(4 / 3)) - np.log(np.log(4)))
         alpha = med + beta * np.log(np.log(2))
         mins = -np.log(-np.log(np.random.rand(self.num_samples))) * beta + alpha
-        m._kill_autoflow()
         self.samples.set_data(mins)
 
     def binary_search(self, left, right, func, val, threshold=0.01):
@@ -92,8 +89,7 @@ class MaxvalueEntropySearch(Acquisition):
         return mid
 
     def build_acquisition(self, Xcand):
-        Xcand = self.models[0].input_transform.build_forward(Xcand)
-        fmean, fvar = self.models[0].wrapped.build_predict(Xcand)
+        fmean, fvar = self.models[0].build_predict(Xcand)
         norm = tf.contrib.distributions.Normal(loc=tf.zeros([], dtype=float_type), scale=tf.ones([], dtype=float_type))
 
         gamma = (fmean - tf.expand_dims(self.samples, axis=0)) / tf.sqrt(fvar)
