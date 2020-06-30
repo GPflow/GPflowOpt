d57fcc18c2efda8a595e6b06beaedbe6b5d716e9
nknudde <nicolas.knudde@ugent.be>
2020-06-30 14:36:41 +0200

    Adapt acquisition.py using tf2

--- gpflowopt/acquisition/acquisition.py
+++ gpflowopt/acquisition/acquisition.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ..domain import UnitCube
 import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
