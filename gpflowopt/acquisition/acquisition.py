9755ba210ad2d226fbb06ca2d1e297a82bfb4393
nknudde <nicolas.knudde@ugent.be>
2020-06-30 14:36:27 +0200

    Adapt acquisition.py using tf2

--- gpflowopt/acquisition/acquisition.py
+++ gpflowopt/acquisition/acquisition.py
@@ -12,49 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ..scaling import DataScaler
 from ..domain import UnitCube
-from ..models import ModelWrapper
-
-from gpflow.param import Parameterized, AutoFlow, ParamList
-from gpflow.model import Model
-from gpflow import settings
-
 import numpy as np
 import tensorflow as tf
-
-import copy
-from functools import wraps
-
-float_type = settings.dtypes.float_type
-
-
-def setup_required(method):
-    """
-    Decorator function to mark methods in Acquisition classes which require running setup if indicated by _needs_setup
-    :param method: acquisition method
-    """
-    @wraps(method)
-    def runnable(instance, *args, **kwargs):
-        assert isinstance(instance, Acquisition)
-        hp = instance.highest_parent
-        if hp._needs_setup:
-            # Avoid infinite loops, caused by setup() somehow invoking the evaluate on another acquisition
-            # e.g. through feasible_data_index.
-            hp._needs_setup = False
-
-            # 1 - optimize
-            hp._optimize_models()
-
-            # 2 - setup
-            hp._setup()
-        results = method(instance, *args, **kwargs)
-        return results
-
-    return runnable
+import tensorflow_probability as tfp
+from gpflowopt.utilities import randomize
+from gpflow.base import Module
+from gpflow.models import BayesianModel
+from gpflow.utilities import parameter_dict, multiple_assign, to_default_float
+from gpflow.models.util import data_input_to_tensor
+import abc
+import gpflow
 
 
-class Acquisition(Parameterized):
+class Acquisition(Module, metaclass=abc.ABCMeta):
     """
     An acquisition function maps the belief represented by a Bayesian model into a
     score indicating how promising a point is for evaluation.
@@ -82,12 +53,11 @@ class Acquisition(Parameterized):
         """
         super(Acquisition, self).__init__()
         models = np.atleast_1d(models)
-        assert all(isinstance(model, (Model, ModelWrapper)) for model in models)
-        self._models = ParamList([DataScaler(m) for m in models])
+        assert all(isinstance(model, BayesianModel) for model in models)
+        self._models = models
 
         assert (optimize_restarts >= 0)
         self.optimize_restarts = optimize_restarts
-        self._needs_setup = True
 
     def _optimize_models(self):
         """
@@ -107,40 +77,25 @@ class Acquisition(Parameterized):
             return
 
         for model in self.models:
-            runs = []
+            loss = []
+            hypers = []
             for i in range(self.optimize_restarts):
                 if i > 0:
-                    model.randomize()
+                    randomize(model)
                 try:
-                    result = model.optimize()
-                    runs.append(result)
+                    opt = gpflow.optimizers.Scipy()
+                    result = opt.minimize(model.training_loss, model.trainable_variables, options=dict(maxiter=1000))
+                    loss.append(result)
+                    hypers.append(parameter_dict(model))
                 except tf.errors.InvalidArgumentError:  # pragma: no cover
                     print("Warning: optimization restart {0}/{1} failed".format(i + 1, self.optimize_restarts))
-            if not runs:
+            if not loss:
                 raise RuntimeError("All model hyperparameter optimization restarts failed, exiting.")
-            best_idx = np.argmin([r.fun for r in runs])
-            model.set_state(runs[best_idx].x)
+            best_idx = np.argmin(loss)
+            multiple_assign(model, hypers[best_idx])
 
     def build_acquisition(self, Xcand):
         raise NotImplementedError
-
-    def enable_scaling(self, domain):
-        """
-        Enables and configures the :class:`.DataScaler` objects wrapping the GP models.
-        
-        Sets the _needs_setup attribute to True so the contained models are optimized and :meth:`setup` is run again
-        right before evaluating the :class:`Acquisition` function. Note that the models are modified directly and
-        references to them outside of the object will also point to scaled instances.
-
-        :param domain: :class:`.Domain` object, the input transform of the data scalers is configured as a transform
-            from domain to the unit cube with the same dimensionality.
-        """
-        n_inputs = self.data[0].shape[1]
-        assert (domain.size == n_inputs)
-        for m in self.models:
-            m.input_transform = domain >> UnitCube(n_inputs)
-            m.normalize_output = True
-        self.highest_parent._needs_setup = True
 
     def set_data(self, X, Y):
         """
@@ -162,10 +117,7 @@ class Acquisition(Parameterized):
             Ypart = Y[:, num_outputs_sum:num_outputs_sum + num_outputs]
             num_outputs_sum += num_outputs
 
-            model.X = X
-            model.Y = Ypart
-
-        self.highest_parent._needs_setup = True
+            model.data = data_input_to_tensor((X, Ypart))
         return num_outputs_sum
 
     @property
@@ -175,7 +127,7 @@ class Acquisition(Parameterized):
         
         :return: list of GPflow models 
         """
-        return self._models.sorted_params
+        return self._models
 
     @property
     def data(self):
@@ -187,10 +139,7 @@ class Acquisition(Parameterized):
 
         :return: tuple X, Y of tensors (if in tf_mode) or numpy arrays.
         """
-        if self._tf_mode:
-            return self.models[0].X, tf.concat(list(map(lambda model: model.Y, self.models)), 1)
-        else:
-            return self.models[0].X.value, np.hstack(map(lambda model: model.Y.value, self.models))
+        return self.models[0].data[0], tf.concat(list(map(lambda model: model.data[1], self.models)), 1)
 
     def constraint_indices(self):
         """
@@ -244,8 +193,7 @@ class Acquisition(Parameterized):
         if self.constraint_indices().size == 0:
             self._setup()
 
-    @setup_required
-    @AutoFlow((float_type, [None, None]))
+    @tf.function
     def evaluate_with_gradients(self, Xcand):
         """
         AutoFlow method to compute the acquisition scores for candidates, also returns the gradients.
@@ -256,8 +204,7 @@ class Acquisition(Parameterized):
         acq = self.build_acquisition(Xcand)
         return acq, tf.gradients(acq, [Xcand], name="acquisition_gradient")[0]
 
-    @setup_required
-    @AutoFlow((float_type, [None, None]))
+    @tf.function
     def evaluate(self, Xcand):
         """
         AutoFlow method to compute the acquisition scores for candidates, without returning the gradients.
@@ -310,7 +257,7 @@ class AcquisitionAggregation(Acquisition):
         """
         super(AcquisitionAggregation, self).__init__()
         assert (all([isinstance(x, Acquisition) for x in operands]))
-        self.operands = ParamList(operands)
+        self.operands = operands
         self._oper = oper
 
     def _optimize_models(self):
@@ -320,10 +267,6 @@ class AcquisitionAggregation(Acquisition):
     @Acquisition.models.getter
     def models(self):
         return [model for acq in self.operands for model in acq.models]
-
-    def enable_scaling(self, domain):
-        for oper in self.operands:
-            oper.enable_scaling(domain)
 
     def set_data(self, X, Y):
         offset = 0
@@ -369,6 +312,7 @@ class AcquisitionSum(AcquisitionAggregation):
     """
     Sum of acquisition functions
     """
+
     def __init__(self, operands):
         super(AcquisitionSum, self).__init__(operands, tf.reduce_sum)
 
@@ -383,6 +327,7 @@ class AcquisitionProduct(AcquisitionAggregation):
     """
     Product of acquisition functions
     """
+
     def __init__(self, operands):
         super(AcquisitionProduct, self).__init__(operands, tf.reduce_prod)
 
@@ -393,7 +338,7 @@ class AcquisitionProduct(AcquisitionAggregation):
             return AcquisitionProduct(self.operands.sorted_params + [other])
 
 
-class MCMCAcquistion(AcquisitionSum):
+class MCMCAcquisition(Acquisition):
     """
     Apply MCMC over the hyperparameters of an acquisition function (= over the hyperparameters of the contained models).
     
@@ -403,56 +348,58 @@ class MCMCAcquistion(AcquisitionSum):
     For evaluating the underlying acquisition function, the predictions of the acquisition copies are averaged.
     """
 
-    def __init__(self, acquisition, n_slices, **kwargs):
-        assert isinstance(acquisition, Acquisition)
-        assert n_slices > 0
+    def __init__(self, operand, num_samples, num_burnin_steps=300, **kwargs):
+
+        assert isinstance(operand, Acquisition)
+        assert num_samples > 0
+        self.operand = operand
         # the call to the constructor of the parent classes, will optimize acquisition, so it obtains the MLE solution.
-        super(MCMCAcquistion, self).__init__([acquisition]*n_slices)
-        self._needs_new_copies = True
+        super(MCMCAcquisition, self).__init__()
         self._sample_opt = kwargs
+        self.num_samples=num_samples
+        self.num_burnin_steps=num_burnin_steps
+        self._samples= None
 
     def _optimize_models(self):
         # Optimize model #1
         self.operands[0]._optimize_models()
 
         # Copy it again if needed due to changed free state
-        if self._needs_new_copies:
-            new_copies = [copy.deepcopy(self.operands[0]) for _ in range(len(self.operands) - 1)]
-            for c in new_copies:
-                c.optimize_restarts = 0
-            self.operands = ParamList([self.operands[0]] + new_copies)
-            self._needs_new_copies = False
-
         # Draw samples using HMC
-        # Sample each model of the acquisition function - results in a list of 2D ndarrays.
-        hypers = np.hstack([model.sample(len(self.operands), **self._sample_opt) for model in self.models])
 
-        # Now visit all acquisition copies, and set state
-        for idx, draw in enumerate(self.operands):
-            draw.set_state(hypers[idx, :])
+        samples = []
+        for model in self.models:
+        # Note that here we need model.trainable_parameters, not trainable_variables - only parameters can have priors!
+            hmc_helper = gpflow.optimizers.SamplingHelper(
+                model.log_posterior_density, model.trainable_parameters
+            )
 
-    @Acquisition.models.getter
-    def models(self):
-        # Only return the models of the first operand, the copies remain hidden.
-        return self.operands[0].models
+            hmc = tfp.mcmc.HamiltonianMonteCarlo(
+                target_log_prob_fn=hmc_helper.target_log_prob_fn, num_leapfrog_steps=10, step_size=0.01
+            )
+            adaptive_hmc = tfp.mcmc.SimpleStepSizeAdaptation(
+                hmc, num_adaptation_steps=10, target_accept_prob=to_default_float(0.75), adaptation_rate=0.1
+            )
 
-    def set_data(self, X, Y):
-        for operand in self.operands:
-            # This triggers model.optimize() on self.operands[0]
-            # All copies have optimization disabled, but must have update data.
-            offset = operand.set_data(X, Y)
-        return offset
+            @tf.function
+            def run_chain_fn():
+                return tfp.mcmc.sample_chain(
+                    num_results=self.num_samples,
+                    num_burnin_steps=self.num_burnin_steps,
+                    current_state=hmc_helper.current_state,
+                    kernel=adaptive_hmc,
+                    trace_fn=lambda _, pkr: pkr.inner_results.is_accepted,
+                )
+
+            sample, _ = run_chain_fn()
+            samples.append([hmc_helper.current_state, sample])
+        self._samples = samples
 
     def build_acquisition(self, Xcand):
         # Average the predictions of the copies.
-        return 1. / len(self.operands) * super(MCMCAcquistion, self).build_acquisition(Xcand)
+        for i in range(self.num_samples):
+            for sample in self._samples:
+                for var, var_samples in sample:
+                    var.assign(var_samples[i])
 
-    def _kill_autoflow(self):
-        """
-        Flag for recreation on next optimize.
-
-        Following the recompilation of models, the free state might have changed. This means updating the samples can
-        cause inconsistencies and errors.
-        """
-        super(MCMCAcquistion, self)._kill_autoflow()
-        self._needs_new_copies = True
+        return 1. / len(self.num_samples) * self.operand.build_acquisition(Xcand)
